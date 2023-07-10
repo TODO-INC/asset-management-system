f5c81b15b1c526047fbf17538f19b21f30c94915
santhosh-vairamuthu <santhosh22032004@gmail.com>
2023-07-10 15:52:26 +0000

    empty container in new

--- assetfrontend/adminpanel/templates/sidebar.php
+++ assetfrontend/adminpanel/templates/sidebar.php
@@ -16,8 +16,8 @@
   </symbol>
 </svg>
 
-<main class="d-flex flex-nowrap">
-<div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="background-color: #002756;width: 230px;">
+
+<div class="d-flex flex-column flex-shrink-0 p-2 bg-body-tertiary" style="background-color: #002756;width: 230px;">
     <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
     <span class="fs-4" style="color: #c6c3b6;font-family: monospace;">ASSET MANAGER</span>
     </a>
@@ -50,7 +50,7 @@
     <hr>
   </div>
   <div class="b-example-divider b-example-vr"></div>
-</main>
+
 <style>
   .bd-placeholder-img {
     font-size: 1.125rem;
