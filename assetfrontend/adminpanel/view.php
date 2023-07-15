22eb0fa8d12eb710b5e9632d6a52c886ea6c2df4
saran-raj007 <k.saranraj000@gmail.com>
2023-07-15 07:01:08 +0530

    update view

--- assetfrontend/adminpanel/view.php
+++ assetfrontend/adminpanel/view.php
@@ -47,15 +47,15 @@ include "libs/load.php";
 </div>
   <div>
     <div class="card">
-    <img src="path/to/image1.jpg" alt="Card Image 1">
+    <img src="image\school.jpg" alt="Card Image 1">
       <p>About school</p>
     <div class="button-container">
-    <button>More</button>
-    <button>Doc</button>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">More</button>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#doc">Doc</button>
     </div>
   </div>
   <div class="card">
-    <img src="path/to/image2.jpg" alt="Card Image 2">
+    <img src="image\school.jpg" alt="Card Image 2">
     <p>About school</p>
     <div class="button-container">
     <button>More</button>
@@ -63,7 +63,7 @@ include "libs/load.php";
     </div>
   </div>
   <div class="card">
-    <img src="path/to/image3.jpg" alt="Card Image 3">
+    <img src="image\school.jpg" alt="Card Image 3">
     <p>About school</p>
     <div class="button-container">
     <button>More</button>
@@ -71,7 +71,7 @@ include "libs/load.php";
     </div>
   </div>
     <div class="card">
-    <img src="path/to/image3.jpg" alt="Card Image 3">
+    <img src="image\school.jpg" alt="Card Image 3">
       <p>About school</p>
     <div class="button-container">
     <button>More</button>
@@ -79,7 +79,7 @@ include "libs/load.php";
     </div>
   </div>
     <div class="card">
-    <img src="path/to/image3.jpg" alt="Card Image 3">
+    <img src="image\school.jpg" alt="Card Image 3">
       <p>About school</p>
     <div class="button-container">
     <button>More</button>
@@ -87,7 +87,7 @@ include "libs/load.php";
     </div>
   </div>
     <div class="card">
-    <img src="path/to/image3.jpg" alt="Card Image 3">
+    <img src="image\school.jpg" alt="Card Image 3">
       <p>About school</p>
     <div class="button-container">
     <button>More</button>
@@ -96,6 +96,130 @@ include "libs/load.php";
   </div>
     
   </div>
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form>
+  <fieldset disabled>
+    <legend>Disabled fieldset example</legend>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Disabled input</label>
+      <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+    </div>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Disabled input</label>
+      <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+    </div>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Disabled input</label>
+      <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+    </div>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Disabled input</label>
+      <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+    </div>
+    <div class="mb-3">
+      <label for="disabledSelect" class="form-label">Disabled select menu</label>
+      <select id="disabledSelect" class="form-select">
+        <option>Disabled select</option>
+      </select>
+    </div>
+    <div class="mb-3">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled>
+        <label class="form-check-label" for="disabledFieldsetCheck">
+          Can't check this
+        </label>
+      </div>
+    </div>
+    
+  </fieldset>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade " id="doc" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+      <div class="accordion-body">
+              <embed
+          src="document.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+          />
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        school2
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <embed
+          src="document.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+          />
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <embed
+          src="document.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+          />
+        
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
   </div>
 </main>
 </body>
