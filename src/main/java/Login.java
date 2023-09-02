7bf9812bbb4643052299b00c6dae5c588668eda0
santhosh <santhosh22032004@gmail.com>
2023-09-02 12:43:05 +0530

    login success

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,35 +5,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
+
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Login() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String uname=request.getParameter("user.name");
+			String password=request.getParameter("user.password");
+			RegisterDao rdao=new RegisterDao();
+			String login="";
+			try {
+				login = rdao.login(uname,password);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(login.equals("True")) {
+				response.sendRedirect("project/index.html?id="+uname);
+			}else {
+				response.getWriter().println(login);
+			}
+			
+			
+		}
 
 }
--- src/main/webapp/project/auth.jsp
+++ src/main/webapp/project/auth.jsp
@@ -66,6 +66,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="js/auth.js"></script>
+<!--<script src="js/auth.js"></script>-->
 </body>
 </html>
